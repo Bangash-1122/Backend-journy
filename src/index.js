ecb90e9981dc6416b4ffd8186749a183a00bdd96
Bangash-1122 <ubaidkhan0077800@gmail.com>
2025-11-30 08:22:59 -0500

    feat: add error handling middleware and 404 response for undefined routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.db.js";
 import { app } from "./app.js";
-import { error } from "console";
+import { ApiError } from "./utils/ApiError.js";        
 dotenv.config({
     path: './.env'
 });
@@ -12,10 +12,6 @@ connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running on port ${process.env.PORT}`);
-        app.listen("errror", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
     });
 })
 .catch((error) => {
